cb0fdde2534689b4d07b6137d79116d76d19dc85
Boet Waldbjorn <boetw@me.com>
2016-12-09 15:07:21 -0800

    map and markers show up

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
-
-  // Intialize our map
-function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 47.580229, lng: -122.313408 },
-          zoom: 16
-        });
-        var infoWindow = new google.maps.InfoWindow({map: map});
-
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-            console.log(pos);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
 $(document).ready(function() {
+   // Intialize our map
+ var center = new google.maps.LatLng(47.580237, -122.382218);
+ var mapOptions = {
+   zoom: 15,
+   center: center,
+   draggable: true
+ };
+ 
+ var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  document.getElementById('dropdown').addEventListener('change', function () {
+    var selectedFeature = document.getElementById('dropdown').value
+ 
   $.ajax({
     url: "https://data.seattle.gov/resource/3c4b-gdxv.json",
     method: "GET",
     datatype: "json",
     data: {
-      "city_feature": "Traffic Cameras"
+      "city_feature": selectedFeature
     }
   }).done(function(data) {
     // Construct a flyout
@@ -60,3 +39,4 @@ $(document).ready(function() {
     });
   });
 });
+ });
