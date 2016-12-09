03ca8f931e40961070aeff0b270896cc9b95422e
BertHaine <bertram.haine@gmail.com>
2016-12-09 12:13:29 -0800

    one marker functional

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
 $(document).ready(function() {
-    // Intialize our map
-  var center = new google.maps.LatLng(47.6475338, -122.3497474);
-  var mapOptions = {
-    zoom: 15,
-    center: center,
-    draggable: false
-  };
-  
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  
-  $.ajax({
-    url: "https://data.seattle.gov/resource/4xy5-26gy.json",
-    method: "GET",
-    datatype: "json",
-    data: {
-       "$where" : "city_feature = parks",
-       "$$app_token" : "Y79BdJhSUWsv9mJb3sYCZzESP"
-    }
-  }).done(function(data) {
-    // Construct a flyout
-    var content = '<div class="flyout">' + '<ul>' 
-        + '<li><em>Date:</em> ' + data[0].date + '</li>' 
-        + '<li><em>Northbound:</em> ' + data[0].fremont_bridge_nb + '</li>' 
-        + '<li><em>Southbound:</em> ' + data[0].fremont_bridge_sb + '</li>' 
-        + '</ul>' + '</div>';
+   // Intialize our map
+ var center = new google.maps.LatLng(47.580237, -122.382218);
+ var mapOptions = {
+   zoom: 15,
+   center: center,
+   draggable: false
+ };
+ 
+ var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+ $.ajax({
+   url: "https://data.seattle.gov/resource/3c4b-gdxv.json",
+   method: "GET",
+   datatype: "json",
+   data: {
+      "city_feature" : "Parks"
+   }
+ }).done(function(data) {
+   // Construct a flyout
+   console.log(data.length);
+   var content = '<div class="flyout">' + '<ul>' 
+       + '<li><em>Name:</em> ' + data[0].common_name + '</li>' 
+       + '<li><em>Website:</em> ' + data[0].website + '</li>' 
+       + '<li><em>Address:</em> ' + data[0].address + '</li>' 
+       + '</ul>' + '</div>';
 
-    var infowindow = new google.maps.InfoWindow({
-      content: content
-    });
-
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(47.6475338, -122.3497474),
-      map: map,
-      title: "Fremont Bridge"
-    });
-    
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, marker);
-        });
-  });
+   var infowindow = new google.maps.InfoWindow({
+     content: content
+   });
+   // var dataObj = JSON.parse(data);
+   // console.log(dataObj);
+   var marker = new google.maps.Marker({
+     position: new google.maps.LatLng(data[0].latitude, data[0].longitude),
+     map: map,
+     title: "Fremont Bridge"
+   });
+   
+   google.maps.event.addListener(marker, 'click', function() {
+       infowindow.open(map, marker);
+       });
+ });
 });
